a56272b9a12c079371e70b1f999c85000f311921
Ben <ben@appsynth.net>
2016-04-28 14:27:46 +0700

    Add new info window

---
+++ TestGoogleMapAPI/SelectionPopup.swift
@@ -0,0 +1,25 @@
+//
+//  SelectionPopup.swift
+//  LineMan
+//
+//  Created by appsynth on 2/16/16.
+//  Copyright © 2016 Appsynth. All rights reserved.
+//
+
+import UIKit
+
+class SelectionPopup: UIView {
+    
+    override func didMoveToSuperview() {
+        superview?.autoresizesSubviews = false;
+    }
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
